5030170607decf5d92710a60740d7730f720b36b
shirin23 <shirin23.dev@gmail.com>
2023-08-09 18:27:27 +0200

    add home about and contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "main#home"
+  
+  get '/home', to: 'main#home'
+  get '/about', to: 'main#about'
+  get '/contact', to: 'main#contact'
+  
+  
 end
